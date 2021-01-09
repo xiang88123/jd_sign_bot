bad47aecef47c19e0e3f878d50091c9586f209ff
xiang88123 <56552774+xiang88123@users.noreply.github.com>
2021-01-09 13:21:01 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,116 +8,69 @@ const rp = require('request-promise');
 const download = require('download');
 
 // 公共变量
-const cookie = process.env.JD_COOKIE;
-const push_key = process.env.PUSH_KEY;
-const dual_cookie = process.env.JD_COOKIE_2;
+const KEY = process.env.JD_COOKIE;
+const serverJ = process.env.PUSH_KEY;
+const DualKey = process.env.JD_COOKIE_2;
 
 
-// 京东脚本文件
-const js_url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js'
-// 下载脚本路劲
-const js_path = './JD_DailyBonus.js'
-// 脚本执行输出路劲
-const result_path = './result.txt'
-// 错误信息输出路劲
-const error_path = './error.txt'
-
-Date.prototype.Format = function (fmt) {
-  var o = {
-    'M+': this.getMonth() + 1,
-    'd+': this.getDate(),
-    'H+': this.getHours(),
-    'm+': this.getMinutes(),
-    's+': this.getSeconds(),
-    'S+': this.getMilliseconds()
-  };
-  if (/(y+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
-  }
-  for (var k in o) {
-    if (new RegExp('(' + k + ')').test(fmt)) {
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (('00' + o[k]).substr(String(o[k]).length)));
-    }
-  }
-  return fmt;
-};
-
-function dateFormat() {
-  var timezone = 8;
-  var GMT_offset = new Date().getTimezoneOffset();
-  var n_Date = new Date().getTime();
-  var t_Date = new Date(n_Date + GMT_offset * 60 * 1000 + timezone * 60 * 60 * 1000);
-  console.log(t_Date)
-  return t_Date.Format('yyyy.MM.dd')
+async function downFile () {
+    // const url = 'https://cdn.jsdelivr.net/gh/NobyDa/Script@master/JD-DailyBonus/JD_DailyBonus.js'
+    const url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js';
+    await download(url, './');
 }
 
-function setupCookie() {
-  var js_content = fs.readFileSync(js_path, 'utf8')
-  js_content = js_content.replace(/var Key = ''/, `var Key = '${cookie}'`)
-  if (dual_cookie) {
-    js_content = js_content.replace(/var DualKey = ''/, `var DualKey = '${dual_cookie}'`)
-  }
-  fs.writeFileSync(js_path, js_content, 'utf8')
+async function changeFile () {
+   let content = await fs.readFileSync('./JD_DailyBonus.js', 'utf8')
+   content = content.replace(/var Key = ''/, `var Key = '${KEY}'`);
+   if (DualKey) {
+    content = content.replace(/var DualKey = ''/, `var DualKey = '${DualKey}'`);
+   }
+   await fs.writeFileSync( './JD_DailyBonus.js', content, 'utf8')
 }
 
-function sendNotificationIfNeed() {
-
-  if (!push_key) {
-    console.log('执行任务结束!'); return;
-  }
-
-  if (!fs.existsSync(result_path)) {
-    console.log('没有执行结果，任务中断!'); return;
-  }
-
-  let text = "京东签到_" + dateFormat();
-  let desp = fs.readFileSync(result_path, "utf8")
-
-  // 去除末尾的换行
-  let SCKEY = push_key.replace(/[\r\n]/g,"")
-
+async function sendNotify (text,desp) {
   const options ={
-    uri:  `https://sc.ftqq.com/${SCKEY}.send`,
+    uri:  `https://sc.ftqq.com/${serverJ}.send`,
     form: { text, desp },
     json: true,
     method: 'POST'
   }
-
-  rp.post(options).then(res=>{
-    const code = res['errno'];
-    if (code == 0) {
-      console.log("通知发送成功，任务结束！")
-    }
-    else {
-      console.log(res);
-      console.log("通知发送失败，任务中断！")
-      fs.writeFileSync(error_path, JSON.stringify(res), 'utf8')
-    }
+  await rp.post(options).then(res=>{
+    console.log(res)
   }).catch((err)=>{
-    console.log("通知发送失败，任务中断！")
-    fs.writeFileSync(error_path, err, 'utf8')
+    console.log(err)
   })
 }
 
-function main() {
-
-  if (!cookie) {
-    console.log('请配置京东cookie!'); return;
+async function start() {
+  if (!KEY) {
+    console.log('请填写 key 后在继续')
+    return
   }
+  // 下载最新代码
+  await downFile();
+  console.log('下载代码完毕')
+  // 替换变量
+  await changeFile();
+  console.log('替换变量完毕')
+  // 执行
+  await exec("node JD_DailyBonus.js >> result.txt");
+  console.log('执行完毕')
 
-  // 1、下载脚本
-  download(js_url, './').then(res=>{
-    // 2、替换cookie
-    setupCookie()
-    // 3、执行脚本
-    exec(`node '${js_path}' >> '${result_path}'`);
-    // 4、发送推送
-    sendNotificationIfNeed() 
-  }).catch((err)=>{
-    console.log('脚本文件下载失败，任务中断！');
-    fs.writeFileSync(error_path, err, 'utf8')
-  })
+  if (serverJ) {
+    const path = "./result.txt";
+    let content = "";
+    if (fs.existsSync(path)) {
+      content = fs.readFileSync(path, "utf8");
+    }
+    let t = content.match(/【签到概览】:((.|\n)*)【签到总计】/)
+    let res = t ? t[1].replace(/\n/,'') : '失败'
+    let t2 = content.match(/【签到总计】:((.|\n)*)【账号总计】/)
+    let res2 = t2 ? t2[1].replace(/\n/,'') : '总计0'
 
+    
+    await sendNotify("" + ` ${res2} ` + ` ${res} ` + new Date().toLocaleDateString(), content);
+  }
 }
 
-main()
+start()
